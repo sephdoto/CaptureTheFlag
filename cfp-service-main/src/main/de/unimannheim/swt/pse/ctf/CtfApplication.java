a66c3a78b56440ded784531ad2a675ddbad84957
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-02-29 13:22:01 +0100

    [#6] Vorgegebene Projekt Grundstruktur

---
+++ cfp-service-main/src/main/de/unimannheim/swt/pse/ctf/CtfApplication.java
@@ -0,0 +1,29 @@
+package de.unimannheim.swt.pse.ctf;
+
+import de.unimannheim.swt.pse.ctf.game.DummyGame;
+import de.unimannheim.swt.pse.ctf.game.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main class (entry point) of your webservice.
+ */
+@SpringBootApplication
+public class CtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfApplication.class, args);
+	}
+
+	/**
+	 * FIXME You need to return your game engine implementation here. Return an instance here.
+	 *
+	 * @return your {@link Game} engine.
+	 */
+	public static Game createGameEngine() {
+		// FIXME change object
+		return new DummyGame();
+	}
+
+}
