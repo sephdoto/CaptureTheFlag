9a529723138189e9e49278f119b19629a9469700
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-03-03 14:53:03 +0100

    [#24] Beschreibung für mapTemplateFolder hinzugefügt

--- cfp-service-main/src/main/de/unimannheim/swt/pse/ctf/constants/Constants.java
+++ cfp-service-main/src/main/de/unimannheim/swt/pse/ctf/constants/Constants.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.nio.file.Paths;
 
 public class Constants {
+	//package map, class JSON_Tools
 	public final static String mapTemplateFolder = Paths.get("").toAbsolutePath().toString().split("cfp14")[0]+"cfp14"+File.separator+"."+File.separator+"cfp-service-main"+File.separator+"src"+File.separator+"main"+File.separator+"resources"+File.separator+"maptemplates"+File.separator;
+	
 	
 }
