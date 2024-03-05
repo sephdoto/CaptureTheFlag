55f290e90ca6e3926f98e9cfd3eb6b162f480d3b
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-03-05 21:34:55 +0100

    [#46] V1 Connection test and responses

---
+++ ctf-TL/src/test/java/org/ctf/TL/ServerCommandTests.java
@@ -0,0 +1,265 @@
+package org.ctf.TL;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.ctf.TL.data.map.MapTemplate;
+import org.ctf.TL.layer.CommLayer;
+import org.ctf.TL.data.wrappers.GameSessionResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+/**
+ * Tests for the layer and the responses it gives out.
+ */
+public class ServerCommandTests {
+    
+    /* Notes while testing
+     * on successful request. Returns gamesessionID, gameover flag and winners;
+     * on fail/malformed request: returns GameOver
+     * malformed always returns gameOver true
+     */
+
+    public static void main (String[] args){
+        testConnection();
+        testMalformedConnection();
+    }
+
+    public static void testConnection(){
+        String jsonPayload = """
+            {
+                "gridSize": [10, 10],
+                "teams": 2,
+                "flags": 1,
+                "blocks": 0,
+                "pieces": [
+                  {
+                    "type": "Pawn",
+                    "attackPower": 1,
+                    "count": 10,
+                    "movement": {
+                      "directions": {
+                        "left": 0,
+                        "right": 0,
+                        "up": 1,
+                        "down": 0,
+                        "upLeft": 1,
+                        "upRight": 1,
+                        "downLeft": 0,
+                        "downRight": 0
+                      }
+                    }
+                  },
+                  {
+                    "type": "Rook",
+                    "attackPower": 5,
+                    "count": 2,
+                    "movement": {
+                      "directions": {
+                        "left": 2,
+                        "right": 2,
+                        "up": 2,
+                        "down": 2,
+                        "upLeft": 0,
+                        "upRight": 0,
+                        "downLeft": 0,
+                        "downRight": 0
+                      }
+                    }
+                  },
+                  {
+                    "type": "Knight",
+                    "attackPower": 3,
+                    "count": 2,
+                    "movement": {
+                      "shape": {
+                        "type": "lshape"
+                      }
+                    }
+                  },
+                  {
+                    "type": "Bishop",
+                    "attackPower": 3,
+                    "count": 2,
+                    "movement": {
+                      "directions": {
+                        "left": 0,
+                        "right": 0,
+                        "up": 0,
+                        "down": 0,
+                        "upLeft": 2,
+                        "upRight": 2,
+                        "downLeft": 2,
+                        "downRight": 2
+                      }
+                    }
+                  },
+                  {
+                    "type": "Queen",
+                    "attackPower": 5,
+                    "count": 1,
+                    "movement": {
+                      "directions": {
+                        "left": 2,
+                        "right": 2,
+                        "up": 2,
+                        "down": 2,
+                        "upLeft": 2,
+                        "upRight": 2,
+                        "downLeft": 2,
+                        "downRight": 2
+                      }
+                    }
+                  },
+                  {
+                    "type": "King",
+                    "attackPower": 1,
+                    "count": 1,
+                    "movement": {
+                      "directions": {
+                        "left": 1,
+                        "right": 1,
+                        "up": 1,
+                        "down": 1,
+                        "upLeft": 1,
+                        "upRight": 1,
+                        "downLeft": 1,
+                        "downRight": 1
+                      }
+                    }
+                  }
+                ],
+                "placement": "symmetrical",
+                "totalTimeLimitInSeconds": -1,
+                "moveTimeLimitInSeconds": -1
+              }
+            """;
+            
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            MapTemplate test = gson.fromJson(jsonPayload, MapTemplate.class);
+    
+            CommLayer testL = new CommLayer("http://localhost:8080");
+            GameSessionResponse testResponse = testL.createGameSession(test);
+            System.out.println(gson.toJson(testResponse));
+    }
+
+    public static void testMalformedConnection(){
+      String jsonPayload = """
+        {
+            "teams": 2,
+            "flags": 1,
+            "blocks": 0,
+            "pieces": [
+              {
+                "type": "Pawn",
+                "attackPower": 1,
+                "count": 10,
+                "movement": {
+                  "directions": {
+                    "left": 0,
+                    "right": 0,
+                    "up": 1,
+                    "down": 0,
+                    "upLeft": 1,
+                    "upRight": 1,
+                    "downLeft": 0,
+                    "downRight": 0
+                  }
+                }
+              },
+              {
+                "type": "Rook",
+                "attackPower": 5,
+                "count": 2,
+                "movement": {
+                  "directions": {
+                    "left": 2,
+                    "right": 2,
+                    "up": 2,
+                    "down": 2,
+                    "upLeft": 0,
+                    "upRight": 0,
+                    "downLeft": 0,
+                    "downRight": 0
+                  }
+                }
+              },
+              {
+                "type": "Knight",
+                "attackPower": 3,
+                "count": 2,
+                "movement": {
+                  "shape": {
+                    "type": "lshape"
+                  }
+                }
+              },
+              {
+                "type": "Bishop",
+                "attackPower": 3,
+                "count": 2,
+                "movement": {
+                  "directions": {
+                    "left": 0,
+                    "right": 0,
+                    "up": 0,
+                    "down": 0,
+                    "upLeft": 2,
+                    "upRight": 2,
+                    "downLeft": 2,
+                    "downRight": 2
+                  }
+                }
+              },
+              {
+                "type": "Queen",
+                "attackPower": 5,
+                "count": 1,
+                "movement": {
+                  "directions": {
+                    "left": 2,
+                    "right": 2,
+                    "up": 2,
+                    "down": 2,
+                    "upLeft": 2,
+                    "upRight": 2,
+                    "downLeft": 2,
+                    "downRight": 2
+                  }
+                }
+              },
+              {
+                "type": "King",
+                "attackPower": 1,
+                "count": 1,
+                "movement": {
+                  "directions": {
+                    "left": 1,
+                    "right": 1,
+                    "up": 1,
+                    "down": 1,
+                    "upLeft": 1,
+                    "upRight": 1,
+                    "downLeft": 1,
+                    "downRight": 1
+                  }
+                }
+              }
+            ],
+            "placement": "symmetrical",
+            "totalTimeLimitInSeconds": -1,
+            "moveTimeLimitInSeconds": -1
+          }
+        """;
+        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        MapTemplate test = gson.fromJson(jsonPayload, MapTemplate.class);
+
+        CommLayer testL = new CommLayer("http://localhost:8080");
+        GameSessionResponse testResponse = testL.createGameSession(test);
+        System.out.println(gson.toJson(testResponse));
+    }
+
+
+}
