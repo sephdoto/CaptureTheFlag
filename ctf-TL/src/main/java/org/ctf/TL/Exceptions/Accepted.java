2ab6254eccab58be45274b9478644d1cbd5e9fab
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-03-05 19:18:05 +0100

    [#41] Moved self created exceptions to Client side. Changed name of Layer.java

---
+++ ctf-TL/src/main/java/org/ctf/TL/Exceptions/Accepted.java
@@ -0,0 +1,14 @@
+package org.ctf.TL.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Represents a special exception (Accepted/OK)
+ * that is marked with a HTTP status if thrown.
+ * @author rsyed
+ */
+@ResponseStatus(value = HttpStatus.OK, reason="OK")
+public class Accepted extends RuntimeException {
+
+}
