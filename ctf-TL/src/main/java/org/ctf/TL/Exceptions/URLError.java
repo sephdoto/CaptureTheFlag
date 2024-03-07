541c3ef6d82e276a71f63ccebeb390132691199a
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-03-07 21:58:16 +0000

    Revert "[#57] Moved Data and Resources to Client Package in prep for the decoupling"

---
+++ ctf-TL/src/main/java/org/ctf/TL/Exceptions/URLError.java
@@ -0,0 +1,16 @@
+package org.ctf.TL.exceptions;
+
+/**
+ * Represents a special exception 404 Not Found (URL Error)
+ * @author rsyed
+ */
+
+public class URLError extends RuntimeException {
+    public URLError(String message){
+        super(message);
+    }
+
+    public URLError(int message){
+        super(Integer.toString(message));
+    }
+}
