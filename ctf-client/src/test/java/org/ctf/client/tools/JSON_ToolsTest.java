96ffbfb14baabc2d29f615693fad9d154b43ce81
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-03-12 09:00:03 +0100

    [#62] JSON_ToolsTest Klasse erstellt

---
+++ ctf-client/src/test/java/org/ctf/client/tools/JSON_ToolsTest.java
@@ -0,0 +1,13 @@
+package org.ctf.client.tools;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class JSON_ToolsTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
