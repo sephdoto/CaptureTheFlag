0cefa7088bf8bfbe1ea1f48c2d454cd17b4dac38
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-04-11 10:35:07 +0200

    #153 Moves Array

--- ctf-client/src/main/java/org/ctf/client/AIClient.java
+++ ctf-client/src/main/java/org/ctf/client/AIClient.java
@@ -1,24 +1,43 @@
 package org.ctf.client;
 
+import java.util.ArrayList;
+
 import org.ctf.client.service.CommLayerInterface;
 import org.ctf.shared.constants.Constants;
+import org.ctf.shared.state.Move;
 
 public class AIClient extends Client implements Runnable {
 
   Constants.AI selectedAI;
-
+  ArrayList<Move> moves;
 
   AIClient(CommLayerInterface comm, String IP, String port) {
     super(comm, IP, port);
+    moves = new ArrayList<Move>();
   }
 
-  public void setSelectedAI(Constants.AI selectedAI){
+  public void setSelectedAI(Constants.AI selectedAI) {
     this.selectedAI = selectedAI;
   }
-  
+
   @Override
   public void run() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'run'");
+    try {
+      // checks if game has a start date and no end date
+      while ((this.getEndDate() == null) && (this.getStartDate() != null)) {
+        //Saves the last move locally
+        Move lastMove = this.getLastMove();
+       
+        if( (this.getLastMove() != null) && (this.getLastMove() != lastMove)) {
+          moves.add(getLastMove());
+        }
+        this.getSessionFromServer();
+        this.getStateFromServer();
+      }
+      Thread.sleep(800);
+    } catch (Exception e) {
+
+    }
+
   }
 }
