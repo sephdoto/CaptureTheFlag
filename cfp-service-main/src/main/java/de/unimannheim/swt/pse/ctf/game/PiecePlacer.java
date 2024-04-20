5d1fd81d5804f1f4f05395d28a049c4718ea5599
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-04-20 21:52:25 +0200

    [#197 #199] symmetrical placement so gut wie fertig
    Es funktioniert, die respawn logik muss aber noch angepasst werden, um
    die boundaries zu respektieren.
    Wie im Diagramm werden zuerst 3 Pieces vor der Base, dann Pieces
    seitlich der Base und zu guter letzt um die Base herum platziert.

--- cfp-service-main/src/main/java/de/unimannheim/swt/pse/ctf/game/PiecePlacer.java
+++ cfp-service-main/src/main/java/de/unimannheim/swt/pse/ctf/game/PiecePlacer.java
@@ -22,13 +22,27 @@ import de.unimannheim.swt.pse.ctf.game.state.Team;
  */
 public class PiecePlacer {
   GameState gameState;
+  //boundaries = a teams rectangular partition of the map, in those boundaries the starter pieces will be placed
   int[][] boundaries;
+  //directions: 
+      // int[0-3] =    where the team is "facing", if it is 0 - left; 1 - right; 2 - up; 3 - down
+      // int[0-3][] =  positions for AI_Tools.updatePos, encoded as mid-left-right
+      // int[4-7] =    where the team is "facing", if it is 4 - left; 5 - right; 6 - up; 7 - down
+      // int[4-7][] =  positions for AI_Tools.updatePos, encoded as left-right
+  int[][] directions = new int[][] {{0,6,4},{1,5,7},{2,4,5},{3,7,6},
+    {3,2},{2,3},{0,1},{1,0}};
   
   public PiecePlacer(GameState gameState, int[][] boundaries) {
     this.gameState = gameState;
     this.boundaries = boundaries;
   }
   
+  /**
+   * This method shall be called to place the Pieces on the grid.
+   * 
+   * @author sistumpf
+   * @param placement
+   */
   public void placePieces(PlacementType placement) {
     switch (placement) {
       case symmetrical:
@@ -44,15 +58,36 @@ public class PiecePlacer {
   }
   
   /**
-   * Places the pieces symmetrical on the board
+   * Places the pieces symmetrical on the grid and in the arrays
    *
    * @author sistumpf
    * @param GameState gameState
    */
   void placePiecesSymmetrical() {
-    for (int team = 0; team < gameState.getTeams().length; team++) {
-      for (int piece = 0; piece < gameState.getTeams()[0].getPieces().length; piece++) {
-        ;
+    for (Team team : gameState.getTeams()) {
+      int facing = nextBaseDirection(team);
+      //place first 3 pieces in front of base
+      int piece = 0;
+      for (; piece < team.getPieces().length && piece<3; piece++) {
+        int[] newPos = posInFrontOfBase(team.getBase(), facing, piece);
+        if(safeToPlace(Integer.parseInt(team.getId()), newPos)) placePiece(team, piece, newPos);
+      }
+      //place to the sides of the base, till boundaries are hit
+      for(int boundsHit=0; boundsHit<2 && piece<team.getPieces().length; piece++) {
+        int reach = (piece-1)/2;
+        int[] newPos = AI_Tools.updatePos(team.getBase().clone(), this.directions[4+facing][(piece+boundsHit)%2], reach);
+        if(safeToPlace(Integer.parseInt(team.getId()), newPos)) {
+          placePiece(team, piece, newPos);
+          boundsHit = 0;
+        }else {
+          piece--;
+          boundsHit++;
+        }
+      }
+      //place remaining pieces with respawn piece logic
+      for(; piece<team.getPieces().length; piece++) {
+        int[] newPos = EngineTools.respawnPiecePosition(gameState, team.getBase());
+        if(safeToPlace(Integer.parseInt(team.getId()), newPos)) placePiece(team, piece, newPos);
       }
     }
   }
@@ -66,10 +101,62 @@ public class PiecePlacer {
   ////////////////////////////////////////////////////
   //        additional helper methods               //
   ////////////////////////////////////////////////////
+  /**
+   * places a piece, given by its index in the team.pieces array, on the position newPos.
+   * 
+   * @author sistumpf
+   * @param team
+   * @param piece
+   * @param newPos
+   */
+  private void placePiece(Team team, int piece, int[] newPos) {
+    team.getPieces()[piece].setPosition(newPos);
+    gameState.getGrid()[newPos[0]][newPos[1]] = team.getPieces()[piece].getId();
+  }
+  
+  /**
+   * Checks if a position is not out of bounds and the position is empty (no pieces/bases/grids)
+   * 
+   * @author sistumpf
+   * @param newPos
+   * @return
+   */
+  private boolean safeToPlace(int team, int[] newPos) {
+    return !positionOutOfBounds(team, newPos) && 
+        AI_Tools.emptyField(gameState.getGrid(), newPos);
+  }
+  
+  /**
+   * Checks if a position is not contained in the teams partition, using this.boundaries
+   *    -> {team index}{lower y, upper y, lower x, upper x}
+   *    
+   * @author sistumpf
+   * @param team index
+   * @param pos position
+   * @return true if the position is out of bounds
+   */
+  public boolean positionOutOfBounds(int team, int[] pos) {
+    return (pos[0] < this.boundaries[team][0] || pos[1] < this.boundaries[team][2] ||
+        pos[0] > this.boundaries[team][1] || pos[1] > this.boundaries[team][3]);
+  }
+  
+  /**
+   * Returns the 3 positions in front of the base, which one depends on midLeftRight
+   * 
+   * @author sistumpf
+   * @param base position
+   * @param facing : in what direction the team is facing:  0 - left; 1 - right; 2 - up; 3 - down
+   * @param midLeftRight : if the position up (0), up-left(1) or up-right(2) is targetted
+   * @return updated position
+   */
+  int[] posInFrontOfBase(int[] base, int facing, int midLeftRight) {
+    return AI_Tools.updatePos(base.clone(), this.directions[facing][midLeftRight], 1);
+  }
   
   /**
    * Returns the direction with the shortest distance to a teams base.
    *
+   * @author sistumpf
    * @param ourBase
    * @param enemyBase
    * @return 0 - left; 1 - right; 2 - up; 3 - down
--- cfp-service-main/src/test/java/de/unimannheim/swt/pse/ctf/game/PiecePlacerTest.java
+++ cfp-service-main/src/test/java/de/unimannheim/swt/pse/ctf/game/PiecePlacerTest.java
@@ -3,11 +3,29 @@ package de.unimannheim.swt.pse.ctf.game;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import de.unimannheim.swt.pse.ctf.game.map.MapTemplate;
+import de.unimannheim.swt.pse.ctf.game.map.PlacementType;
 import de.unimannheim.swt.pse.ctf.game.state.GameState;
 import de.unimannheim.swt.pse.ctf.game.state.Team;
 
+/**
+ * @author sistumpf
+ */
 class PiecePlacerTest {
-
+  @Test
+  void testPlacePiecesSymmetrical() {
+    GameState gs = TestValues.getTestState();
+    MapTemplate mt = TestValues.getTestTemplate();
+    mt.setGridSize(new int[] {30,30});
+    mt.setTeams(3);
+    gs.setTeams(new Team[3]);
+    BoardController bc = new BoardController(gs, mt);
+    for(int i=0; i<bc.gameState.getTeams().length; i++)
+      bc.initializeTeam(i, mt);
+    PiecePlacer pp = new PiecePlacer(bc.gameState, bc.boundaries);
+    pp.placePieces(PlacementType.symmetrical);
+    printGrid(pp.gameState);
+  }
+  
   @Test
   void testNextBaseDirection() {
     BoardController bc = new BoardController(TestValues.getTestState(), TestValues.getTestTemplate());
@@ -33,7 +51,7 @@ class PiecePlacerTest {
   void printGrid(GameState gameState) {
     for(int y=0; y<gameState.getGrid().length; y++) {
       for(int x=0; x<gameState.getGrid()[y].length; x++)
-        System.out.print(gameState.getGrid()[y][x].equals("") ? ".    " : gameState.getGrid()[y][x] + " ");
+        System.out.print(gameState.getGrid()[y][x].equals("") ? ".     " : gameState.getGrid()[y][x] + " ");
       System.out.println();
     }
   }
