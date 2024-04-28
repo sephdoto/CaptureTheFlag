9d45de5272464a1cd0bb54352e410da7d611afe3
Aaron Niemesch <aaron.juergen.niemesch@students.uni-mannheim.de>
2024-04-28 21:26:00 +0200

    [#214] Added Documentation for PopUpPane

--- ctf-ui/src/main/java/org/ctf/ui/customobjects/PopUpPane.java
+++ ctf-ui/src/main/java/org/ctf/ui/customobjects/PopUpPane.java
@@ -4,8 +4,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 
+/**
+ * Provides a custom Container that functions as a internal pop up window
+ * which disables the background and that can be used in different parts of 
+ * the application.
+ * 
+ *  @author aniemesc
+ */
 public class PopUpPane extends StackPane {
 	StackPane popUp;
+	
+	/**
+	 * Initializes and styles the custom PopUpPane.
+	 * 
+	 * @param scene - Scene on which PopUpPane should be placed
+	 * @param widthRatio - Percentage of Width that PopUpPane should cover
+	 * @param heightRatio - Percentage of Height that PopUpP
+	 */
 	public PopUpPane(Scene scene,double width,double height) {
 		this.getStyleClass().add("blur-pane");
 		popUp = new StackPane();
@@ -14,9 +29,22 @@ public class PopUpPane extends StackPane {
 		popUp.getStyleClass().add("pop-up-pane");
 		this.getChildren().add(popUp);
 	}
+	
+	/**
+	 * Sets the content of a PopUpPane.
+	 * 
+	 * @author aniemesc
+	 * @param parent - Parent object
+	 */
 	public void setContent(Parent parent) {
 		popUp.getChildren().add(parent);
 	}
+	
+	/**
+	 * Returns the internal Container of the PopUpPane.
+	 * @author aniemesc
+	 * @return
+	 */
 	public StackPane getPopUp() {
 		return popUp;
 	}
