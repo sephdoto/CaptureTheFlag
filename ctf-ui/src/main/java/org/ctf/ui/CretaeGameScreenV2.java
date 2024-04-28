b2a90c6620c992b41fc99717aa35290e8d816209
Manuel Krakowski <manuel.niklas.krakowski@students.uni-mannheim.de>
2024-04-29 00:12:10 +0200

    [#224]

--- ctf-ui/src/main/java/org/ctf/ui/CretaeGameScreenV2.java
+++ ctf-ui/src/main/java/org/ctf/ui/CretaeGameScreenV2.java
@@ -52,7 +52,6 @@ public class CretaeGameScreenV2 extends Scene {
 	private ObjectProperty<Font> leaveButtonText = new SimpleObjectProperty<Font>(Font.getDefault());
 	private ObjectProperty<Font> aiPowerText = new SimpleObjectProperty<Font>(Font.getDefault());
 
-
 	public CretaeGameScreenV2(HomeSceneController hsc, double width, double height) {
 		super(new StackPane(), width, height);
 		this.hsc = hsc;
@@ -62,9 +61,9 @@ public class CretaeGameScreenV2 extends Scene {
 		this.root = (StackPane) this.getRoot();
 		createLayout();
 	}
-	
+
 	private void createChooserPopup() {
-	 pop = new PopUpPane(this, 0.5, 0.3);
+		pop = new PopUpPane(this, 0.5, 0.3);
 		root.getChildren().add(pop);
 		VBox top = new VBox();
 		top.heightProperty().addListener((obs, oldVal, newVal) -> {
@@ -88,18 +87,18 @@ public class CretaeGameScreenV2 extends Scene {
 		Button ai = createAddAIButton("Play as AI", "robot1.png");
 		ai.prefWidthProperty().bind(human.widthProperty());
 		ai.prefHeightProperty().bind(human.heightProperty());
-		chooseButtonBox.getChildren().addAll(human,ai );
+		chooseButtonBox.getChildren().addAll(human, ai);
 		top.getChildren().add(chooseButtonBox);
 		HBox centerLeaveButton = new HBox();
-		//centerLeaveButton.setStyle("-fx-background-color: blue");
+		// centerLeaveButton.setStyle("-fx-background-color: blue");
 		centerLeaveButton.prefHeightProperty().bind(pop.heightProperty().multiply(0.4));
 		centerLeaveButton.setAlignment(Pos.CENTER);
 		centerLeaveButton.getChildren().add(createCancelButton());
 		top.getChildren().add(centerLeaveButton);
 		pop.setContent(top);
 	}
-	
-	private void createAiLevelPopUp(){
+
+	private void createAiLevelPopUp() {
 		root.getChildren().remove(pop);
 		portText.setDisable(true);
 		serverIPText.setDisable(true);
@@ -118,31 +117,38 @@ public class CretaeGameScreenV2 extends Scene {
 		HBox buttonBox = new HBox();
 		buttonBox.setAlignment(Pos.CENTER);
 		pop2.widthProperty().addListener((observable, oldValue, newValue) -> {
-			double newSpacing = newValue.doubleValue() * 0.03; 
-			double padding = newValue.doubleValue()* 0.03;
+			double newSpacing = newValue.doubleValue() * 0.03;
+			double padding = newValue.doubleValue() * 0.03;
 			buttonBox.setSpacing(newSpacing);
 			buttonBox.setPadding(new Insets(0, padding, 0, padding));
 		});
-		buttonBox.getChildren().addAll(createAIPowerButton("easy", "green"), createAIPowerButton("medium", "yellow"),createAIPowerButton("strong", "x"));
+		buttonBox.getChildren().addAll(createAIPowerButton("easy"), createAIPowerButton("medium"),
+				createAIPowerButton("strong"));
 		top.getChildren().add(buttonBox);
 		HBox centerLeaveButton = new HBox();
-		//centerLeaveButton.setStyle("-fx-background-color: blue");
+		// centerLeaveButton.setStyle("-fx-background-color: blue");
 		centerLeaveButton.prefHeightProperty().bind(pop.heightProperty().multiply(0.2));
 		centerLeaveButton.setAlignment(Pos.CENTER);
 		centerLeaveButton.getChildren().add(createBackButton());
 		top.getChildren().add(centerLeaveButton);
 		pop2.setContent(top);
 		root.getChildren().add(pop2);
-		
+
 	}
-	
-	
-	private Button createAIPowerButton(String pow, String color) {
+
+	private Button createAIPowerButton(String pow) {
 		Button power = new Button(pow);
-		
 		power.fontProperty().bind(aiPowerText);
-		power.getStyleClass().add("ai-button");
-		power.setStyle("-fx-backround-color:" + color);
+		if (pow.equals("easy")) {
+			power.getStyleClass().add("ai-button-easy");
+		}
+		if (pow.equals("medium")) {
+			power.getStyleClass().add("ai-button-medium");
+		}
+		if (pow.equals("strong")) {
+			power.getStyleClass().add("ai-button-strong");
+		}
+		
 		power.prefWidthProperty().bind(root.widthProperty().multiply(0.15));
 		power.prefHeightProperty().bind(power.widthProperty().multiply(0.35));
 		power.setOnAction(e -> {
@@ -150,8 +156,7 @@ public class CretaeGameScreenV2 extends Scene {
 		});
 		return power;
 	}
-	
-	
+
 	private Button createCancelButton() {
 		Button exit = new Button("Cancel");
 		exit.fontProperty().bind(leaveButtonText);
@@ -165,7 +170,7 @@ public class CretaeGameScreenV2 extends Scene {
 		serverIPText.setDisable(false);
 		return exit;
 	}
-	
+
 	private Button createBackButton() {
 		Button exit = new Button("back");
 		exit.fontProperty().bind(leaveButtonText);
@@ -173,23 +178,21 @@ public class CretaeGameScreenV2 extends Scene {
 		exit.prefWidthProperty().bind(root.widthProperty().multiply(0.1));
 		exit.prefHeightProperty().bind(exit.widthProperty().multiply(0.25));
 		exit.setOnAction(e -> {
-			root.getChildren().add(pop); 
+			root.getChildren().add(pop);
 		});
 		return exit;
 	}
-	
+
 	private void manageFontSizes() {
-		 widthProperty().addListener(new ChangeListener<Number>()
-		    {
-		        public void changed(ObservableValue<? extends Number> observableValue, Number oldWidth, Number newWidth)
-		        {
-		            addHumanButtonTextFontSIze.set(Font.font(newWidth.doubleValue()/ 70));
-		            addAiCOmboTextFontSIze.set(Font.font(newWidth.doubleValue()/ 60));
-		            popUpLabel.set(Font.font(newWidth.doubleValue()/ 60));
-		            leaveButtonText.set(Font.font(newWidth.doubleValue()/ 80));
-		            aiPowerText.set(Font.font(newWidth.doubleValue()/ 50));
-		        }
-		    });
+		widthProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue<? extends Number> observableValue, Number oldWidth, Number newWidth) {
+				addHumanButtonTextFontSIze.set(Font.font(newWidth.doubleValue() / 70));
+				addAiCOmboTextFontSIze.set(Font.font(newWidth.doubleValue() / 60));
+				popUpLabel.set(Font.font(newWidth.doubleValue() / 60));
+				leaveButtonText.set(Font.font(newWidth.doubleValue() / 80));
+				aiPowerText.set(Font.font(newWidth.doubleValue() / 50));
+			}
+		});
 	}
 
 	private void createLayout() {
@@ -208,22 +211,20 @@ public class CretaeGameScreenV2 extends Scene {
 		left = createOptionPane();
 		selected = StroeMaps.getRandomMapName();
 		right = createShowMapPane(selected);
-		
+
 		sep.getChildren().add(left);
 		sep.getChildren().add(right);
 		mainBox.getChildren().add(sep);
-		//mainBox.getChildren().add(createSettings());
+		// mainBox.getChildren().add(createSettings());
 		mainBox.getChildren().add(createLeave());
 		this.widthProperty().addListener((observable, oldValue, newValue) -> {
-			double newSpacing = newValue.doubleValue() * 0.05; 
+			double newSpacing = newValue.doubleValue() * 0.05;
 			sep.setSpacing(newSpacing);
 		});
 		left.getChildren().add(createLeftcontent());
-		
-
-
 
 	}
+
 	private Button createLeave() {
 		Button exit = new Button("Leave");
 		exit.getStyleClass().add("leave-button");
@@ -235,9 +236,7 @@ public class CretaeGameScreenV2 extends Scene {
 		});
 		return exit;
 	}
-	
-	
-	
+
 //	private ImageView createSettings() {
 //		Image mp = new Image(getClass().getResourceAsStream("Settings_(iOS).png"));
 //		ImageView mpv = new ImageView(mp);
@@ -245,7 +244,7 @@ public class CretaeGameScreenV2 extends Scene {
 //		mpv.setPreserveRatio(true);
 //		return mpv;
 //	}
-	
+
 	private ImageView createHeader() {
 		Image mp = new Image(getClass().getResourceAsStream("multiplayerlogo.png"));
 		ImageView mpv = new ImageView(mp);
@@ -262,12 +261,11 @@ public class CretaeGameScreenV2 extends Scene {
 		});
 		return mpv;
 	}
-	
-	
+
 	private VBox createLeftcontent() {
 		VBox leftBox = new VBox();
 		leftBox.setAlignment(Pos.TOP_CENTER);
-		//leftBox.setPadding(new Insets(20));
+		// leftBox.setPadding(new Insets(20));
 		leftBox.setSpacing(left.heightProperty().doubleValue() * 0.06);
 		left.heightProperty().addListener((obs, oldVal, newVal) -> {
 			double spacing = newVal.doubleValue() * 0.06;
@@ -287,12 +285,12 @@ public class CretaeGameScreenV2 extends Scene {
 		enterSeverInfoBox.prefHeightProperty().bind(serverInfoBox.heightProperty().multiply(0.6));
 		enterSeverInfoBox.prefWidthProperty().bind(serverInfoBox.widthProperty());
 		enterSeverInfoBox.setAlignment(Pos.CENTER);
-		enterSeverInfoBox.setSpacing(enterSeverInfoBox.widthProperty().doubleValue()*0.06);
+		enterSeverInfoBox.setSpacing(enterSeverInfoBox.widthProperty().doubleValue() * 0.06);
 		enterSeverInfoBox.widthProperty().addListener((obs, oldVal, newVal) -> {
 			double spacing = newVal.doubleValue() * 0.06;
 			enterSeverInfoBox.setSpacing(spacing);
 		});
-		 serverIPText = createTextfield("Enter the Server IP");
+		serverIPText = createTextfield("Enter the Server IP");
 		serverIPText.prefWidthProperty().bind(enterSeverInfoBox.widthProperty().multiply(0.4));
 		enterSeverInfoBox.getChildren().add(serverIPText);
 		portText = createTextfield("Enter the Port");
@@ -300,15 +298,14 @@ public class CretaeGameScreenV2 extends Scene {
 		enterSeverInfoBox.getChildren().add(portText);
 		serverInfoBox.getChildren().add(enterSeverInfoBox);
 		leftBox.getChildren().add(serverInfoBox);
-		
-		
+
 		VBox buttonBox = new VBox();
 		buttonBox.getStyleClass().add("option-pane");
 		buttonBox.prefHeightProperty().bind(left.heightProperty().multiply(0.7));
 		buttonBox.getChildren().add(createHeader(leftBox, "Choose Map"));
 		buttonBox.setAlignment(Pos.CENTER);
 		buttonBox.setPadding(new Insets(20));
-		//buttonBox.setSpacing(buttonBox.heightProperty().doubleValue() * 0.06);
+		// buttonBox.setSpacing(buttonBox.heightProperty().doubleValue() * 0.06);
 		buttonBox.heightProperty().addListener((obs, oldVal, newVal) -> {
 			double spacing = newVal.doubleValue() * 0.2;
 			buttonBox.setSpacing(spacing);
@@ -316,9 +313,10 @@ public class CretaeGameScreenV2 extends Scene {
 		buttonBox.getChildren().add(createChoiceBox(buttonBox));
 		buttonBox.getChildren().add(createCreateButton());
 		leftBox.getChildren().add(buttonBox);
-		
+
 		return leftBox;
 	}
+
 	private Button createCreateButton() {
 		Button search = new Button("Create");
 		search.getStyleClass().add("leave-button");
@@ -327,13 +325,13 @@ public class CretaeGameScreenV2 extends Scene {
 		search.fontProperty().bind(Bindings.createObjectBinding(
 				() -> Font.font("Century Gothic", search.getHeight() * 0.4), search.heightProperty()));
 		search.setOnAction(e -> {
-			if(portText.getText().isEmpty() ) {
+			if (portText.getText().isEmpty()) {
 				informationmustBeEntered(portText);
 			}
-			if(serverIPText.getText().isEmpty()) {
+			if (serverIPText.getText().isEmpty()) {
 				informationmustBeEntered(serverIPText);
 			}
-			if(!portText.getText().isEmpty() && !serverIPText.getText().isEmpty() ) {
+			if (!portText.getText().isEmpty() && !serverIPText.getText().isEmpty()) {
 				serverIP = serverIPText.getText();
 				port = portText.getText();
 				hsc.setPort(port);
@@ -342,27 +340,24 @@ public class CretaeGameScreenV2 extends Scene {
 				this.createChooserPopup();
 			}
 		});
-		 
+
 		return search;
 	}
-	
-	
 
 	private void informationmustBeEntered(TextField t) {
 		t.getStyleClass().add("custom-search-field2-mustEnter");
 		TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.05), t);
-        translateTransition.setFromX(-2);
-        translateTransition.setToX(2);
-        translateTransition.setCycleCount(10);
-        translateTransition.setAutoReverse(true);
-        translateTransition.playFromStart();
-        translateTransition.setOnFinished(event -> {
-        	t.getStyleClass().remove("custom-search-field2-mustEnter");
-	        t.getStyleClass().add("custom-search-field2");
-        });
+		translateTransition.setFromX(-2);
+		translateTransition.setToX(2);
+		translateTransition.setCycleCount(10);
+		translateTransition.setAutoReverse(true);
+		translateTransition.playFromStart();
+		translateTransition.setOnFinished(event -> {
+			t.getStyleClass().remove("custom-search-field2-mustEnter");
+			t.getStyleClass().add("custom-search-field2");
+		});
 	}
-	
-	
+
 	private Button createAddHumanButton(String text, String src) {
 		Button button = new Button(text);
 		button.getStyleClass().add("button25");
@@ -370,16 +365,16 @@ public class CretaeGameScreenV2 extends Scene {
 		Image mp = new Image(getClass().getResourceAsStream(src));
 		ImageView vw = new ImageView(mp);
 		button.setGraphic(vw);
-        button.setContentDisplay(ContentDisplay.RIGHT);
-        vw.fitWidthProperty().bind(button.widthProperty().divide(5));
-        vw.setPreserveRatio(true);
-        button.setMaxWidth(Double.MAX_VALUE); 
-        button.setOnAction(e -> {
+		button.setContentDisplay(ContentDisplay.RIGHT);
+		vw.fitWidthProperty().bind(button.widthProperty().divide(5));
+		vw.setPreserveRatio(true);
+		button.setMaxWidth(Double.MAX_VALUE);
+		button.setOnAction(e -> {
 			hsc.switchToWaitGameScene(App.getStage());
 		});
-        return button;
+		return button;
 	}
-	
+
 	private Button createAddAIButton(String text, String src) {
 		Button button = new Button(text);
 		button.getStyleClass().add("button25");
@@ -387,17 +382,17 @@ public class CretaeGameScreenV2 extends Scene {
 		Image mp = new Image(getClass().getResourceAsStream(src));
 		ImageView vw = new ImageView(mp);
 		button.setGraphic(vw);
-        button.setContentDisplay(ContentDisplay.RIGHT);
-        vw.fitWidthProperty().bind(button.widthProperty().divide(8));
-        vw.setPreserveRatio(true);
-        button.setMaxWidth(Double.MAX_VALUE);
-    	button.setOnAction(e -> {
+		button.setContentDisplay(ContentDisplay.RIGHT);
+		vw.fitWidthProperty().bind(button.widthProperty().divide(8));
+		vw.setPreserveRatio(true);
+		button.setMaxWidth(Double.MAX_VALUE);
+		button.setOnAction(e -> {
 			createAiLevelPopUp();
 		});
-        return button;
-        
+		return button;
+
 	}
-	
+
 	private Text createHeader(VBox leftBox, String text) {
 		Text leftheader = new Text(text);
 		leftheader.getStyleClass().add("custom-header");
@@ -405,17 +400,17 @@ public class CretaeGameScreenV2 extends Scene {
 				.bind(Bindings.createObjectBinding(() -> Font.font(leftBox.getWidth() / 18), leftBox.widthProperty()));
 		return leftheader;
 	}
-	
+
 	public StackPane createOptionPane() {
 		StackPane pane = new StackPane();
-		//pane.getStyleClass().add("option-pane");
+		// pane.getStyleClass().add("option-pane");
 		pane.setPrefSize(250, 250);
 		pane.prefWidthProperty().bind(this.widthProperty().multiply(0.4));
 		pane.prefHeightProperty().bind(pane.widthProperty().multiply(0.8));
 
 		return pane;
 	}
-	
+
 	private TextField createTextfield(String prompt) {
 		TextField searchField = new TextField();
 		searchField.getStyleClass().add("custom-search-field2");
@@ -427,56 +422,56 @@ public class CretaeGameScreenV2 extends Scene {
 		});
 		return searchField;
 	}
-	
-	private void fitTextfield(TextField tf, Label lbl, double max ) {
-	     double defaultFontSize = 32;
-	     Font defaultFont = Font.font(defaultFontSize);
-        lbl.setFont(defaultFont);
-        lbl.textProperty().addListener((observable, oldValue, newValue) -> {
-            
-            Text tmpText = new Text(newValue);
-            tmpText.setFont(defaultFont);
 
-            double textWidth = tmpText.getLayoutBounds().getWidth();
-            if (textWidth <= max) {
-                lbl.setFont(defaultFont);
-            } else {
-               
-                double newFontSize = defaultFontSize * max / textWidth;
-                lbl.setFont(Font.font(defaultFont.getFamily(), newFontSize));
-            }
+	private void fitTextfield(TextField tf, Label lbl, double max) {
+		double defaultFontSize = 32;
+		Font defaultFont = Font.font(defaultFontSize);
+		lbl.setFont(defaultFont);
+		lbl.textProperty().addListener((observable, oldValue, newValue) -> {
 
-        });
-        lbl.textProperty().bind(tf.textProperty());
+			Text tmpText = new Text(newValue);
+			tmpText.setFont(defaultFont);
+
+			double textWidth = tmpText.getLayoutBounds().getWidth();
+			if (textWidth <= max) {
+				lbl.setFont(defaultFont);
+			} else {
+
+				double newFontSize = defaultFontSize * max / textWidth;
+				lbl.setFont(Font.font(defaultFont.getFamily(), newFontSize));
+			}
+
+		});
+		lbl.textProperty().bind(tf.textProperty());
 	}
-	
-	private  ComboBox<String> createChoiceBox(VBox parent) {
+
+	private ComboBox<String> createChoiceBox(VBox parent) {
 		ComboBox<String> c = new ComboBox<String>();
 		c.getStyleClass().add("combo-box");
-		 c.setCellFactory(param -> new ListCell<String>() {
-	            @Override
-	            protected void updateItem(String item, boolean empty) {
-	                super.updateItem(item, empty);
-	                if (empty || item == null) {
-	                    setText(null);
-	                } else {
-	                    setText(item);
-	                    setAlignment(javafx.geometry.Pos.CENTER); 
-	                }
-	            }
-	        });
-		 c.setButtonCell(new ListCell<String>() {
-			    @Override
-			    protected void updateItem(String item, boolean empty) {
-			        super.updateItem(item, empty);
-			        if (empty || item == null) {
-			            setText(null);
-			        } else {
-			            setText(item);
-			            setAlignment(javafx.geometry.Pos.CENTER); 
-			        }
-			    }
-			});
+		c.setCellFactory(param -> new ListCell<String>() {
+			@Override
+			protected void updateItem(String item, boolean empty) {
+				super.updateItem(item, empty);
+				if (empty || item == null) {
+					setText(null);
+				} else {
+					setText(item);
+					setAlignment(javafx.geometry.Pos.CENTER);
+				}
+			}
+		});
+		c.setButtonCell(new ListCell<String>() {
+			@Override
+			protected void updateItem(String item, boolean empty) {
+				super.updateItem(item, empty);
+				if (empty || item == null) {
+					setText(null);
+				} else {
+					setText(item);
+					setAlignment(javafx.geometry.Pos.CENTER);
+				}
+			}
+		});
 		c.getItems().addAll(StroeMaps.getValues());
 		c.setValue(selected);
 		c.prefWidthProperty().bind(parent.widthProperty().multiply(0.8));
@@ -486,7 +481,7 @@ public class CretaeGameScreenV2 extends Scene {
 			sep.getChildren().remove(right);
 			right = createShowMapPane(selected);
 			sep.getChildren().add(right);
-			
+
 		});
 		return c;
 	}
@@ -495,7 +490,7 @@ public class CretaeGameScreenV2 extends Scene {
 		StackPane showMapBox = new StackPane();
 		showMapBox.getStyleClass().add("option-pane");
 		showMapBox.prefWidthProperty().bind(this.widthProperty().multiply(0.4));
-		//showMapBox.setStyle("-fx-background-color: white");
+		// showMapBox.setStyle("-fx-background-color: white");
 		showMapBox.prefHeightProperty().bind(showMapBox.widthProperty());
 		showMapBox.getStyleClass().add("show-GamePane");
 		state = StroeMaps.getMap(name);
@@ -503,6 +498,7 @@ public class CretaeGameScreenV2 extends Scene {
 		showMapBox.getChildren().add(gm);
 		return showMapBox;
 	}
+
 	public String getServerIP() {
 		return serverIP;
 	}
@@ -527,5 +523,3 @@ public class CretaeGameScreenV2 extends Scene {
 		this.serverManager = serverManager;
 	}
 }
-
-	
