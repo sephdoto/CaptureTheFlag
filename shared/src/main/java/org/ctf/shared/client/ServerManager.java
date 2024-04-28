0e1379004a30d572422316961b905d6bad70f57f
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-04-28 17:51:05 +0200

    #232 JavaDoc and bugfixes

--- shared/src/main/java/org/ctf/shared/client/ServerManager.java
+++ shared/src/main/java/org/ctf/shared/client/ServerManager.java
@@ -6,20 +6,42 @@ import org.ctf.shared.state.data.map.MapTemplate;
 import org.ctf.shared.state.dto.GameSessionRequest;
 import org.ctf.shared.state.dto.GameSessionResponse;
 
+/**
+ * Abstraction which servers as a Manager for creating and deleting Sessions. Removes the need for
+ * the Client to make sessions leading to simplifications in the UI flow.
+ *
+ * @author rsyed
+ */
 public class ServerManager {
   CommLayerInterface comm;
-  public ServerDetails serverInfo;
+  public ServerDetails serverDetails;
   MapTemplate map;
   public String currentServer;
   public String gameSessionID;
 
-  public ServerManager(CommLayerInterface comm, ServerDetails serverInfo, MapTemplate map) {
+  /**
+   * Constructor
+   *
+   * @param comm An object of {@link CommLayerInterface}. Either CommLayer or RestClientLayer
+   * @param serverInfo An object of {@link ServerDetails}. Contains URL and Port information
+   * @param map An object of {@link MapTemplate}. Is the map it will use to create the Session.
+   * @author rsyed
+   */
+  public ServerManager(CommLayerInterface comm, ServerDetails serverDetails, MapTemplate map) {
     this.comm = comm;
     this.map = map;
+    this.serverDetails = serverDetails;
     this.currentServer =
-        "http://" + serverInfo.getHost() + ":" + serverInfo.getPort() + "/api/gamesession";
+        "http://" + serverDetails.getHost() + ":" + serverDetails.getPort() + "/api/gamesession";
+        
   }
 
+  /**
+   * Creates a game session in the server set in the object.
+   *
+   * @return True on Success, False otherwise
+   * @author rsyed
+   */
   public boolean createGame() {
     GameSessionRequest gsr = new GameSessionRequest();
     gsr.setTemplate(map);
@@ -31,6 +53,12 @@ public class ServerManager {
     return gSessionResponse.getId() != null;
   }
 
+  /**
+   * Deletes the game session held in the object
+   *
+   * @return True on Success, False otherwise
+   * @author rsyed
+   */
   public boolean deleteGame() {
     try {
       comm.deleteCurrentSession(currentServer + "/" + gameSessionID);
@@ -40,7 +68,15 @@ public class ServerManager {
     return true;
   }
 
+  //Getters and Setters
   public String getGameSessionID() {
     return this.gameSessionID;
+  }
+  public void setMapTemplate(MapTemplate mapTemplate){
+    this.map = mapTemplate;
+  }
+
+  public void setServer(ServerDetails serverDetails){
+    this.serverDetails = serverDetails;
   }
 }
