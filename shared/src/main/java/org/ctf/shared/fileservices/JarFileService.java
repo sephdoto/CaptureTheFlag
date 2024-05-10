1f5f8e4d20596aa18748a596dc99341df33b7c40
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-05-10 20:02:29 +0200

    #265 TerminalCommandService

---
+++ shared/src/main/java/org/ctf/shared/fileservices/JarFileService.java
@@ -0,0 +1,27 @@
+package org.ctf.shared.fileservices;
+
+import java.io.File;
+
+/**
+ * Service file providing functionality to load and save resources into JAR files. Used when we
+ * export the project as a JAR
+ *
+ * @author rsyed
+ */
+public class JarFileService implements JARFileServices {
+
+  @Override
+  public File getFile(String fileName, String extension) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getFile'");
+  }
+
+  @Override
+  public boolean saveFile(File file, String dir, String fileName, String extension) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'saveFile'");
+  }
+
+
+  
+}
---
+++ shared/src/main/java/org/ctf/shared/fileservices/TerminalCommandService.java
@@ -0,0 +1,34 @@
+package org.ctf.shared.fileservices;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class TerminalCommandService {
+  static final String SPACE = " ";
+  Logger logger = Logger.getLogger(getClass().getName());
+
+  public static void main(String[] args) {
+    listJarFiles("F:", "testjar.jar");
+  }
+
+  public static void addFileToJar(File file, String path) {
+    String commandBase =  "jar" + SPACE + "uf" + SPACE + "path";
+    runCommandinShell(commandBase);
+  }
+
+  public static void listJarFiles(String path, String fileName) {
+    String commandBase = "jar" + SPACE + "tf" + SPACE + path + "\\" + fileName;
+    runCommandinShell(commandBase);
+  }
+
+  private static void runCommandinShell(String command) {
+    try {
+      Process proc = Runtime.getRuntime().exec(command);
+      proc.getInputStream().transferTo(System.out);
+      proc.getErrorStream().transferTo(System.out);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
