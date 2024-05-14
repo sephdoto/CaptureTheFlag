a55ec8d1378e8fc5516d0def198ca313cab28ec0
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-05-14 09:28:07 +0200

    [#271] Tests für unnormalizeMove eingefügt

--- shared/src/test/java/org/ctf/shared/ai/GameStateNormalizerTest.java
+++ shared/src/test/java/org/ctf/shared/ai/GameStateNormalizerTest.java
@@ -1,6 +1,9 @@
 package org.ctf.shared.ai;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.ctf.shared.ai.GameUtilities.InvalidShapeException;
+import org.ctf.shared.ai.GameUtilities.NoMovesLeftException;
+import org.ctf.shared.ai.random.RandomAI;
 import org.ctf.shared.state.GameState;
 import org.ctf.shared.state.Move;
 import org.junit.jupiter.api.Test;
@@ -19,21 +22,49 @@ class GameStateNormalizerTest {
   @Test
   void testNormalize() {
     //create unnormalized GameState
-    GameState gs = TestValues.getTestState();
-    for(int i=0; i<gs.getTeams().length; i++) {
-      gs.getTeams()[i].setId(funIds[i]);
-      for(int j=0; j<gs.getTeams()[i].getPieces().length; j++) {
-        gs.getTeams()[i].getPieces()[j].setId("p:"+ "" + "_" + j+Math.random() * 80085);
-      }
-    }
+    GameState gs = unnormalizedGameState();
     
     GameStateNormalizer gsn = new GameStateNormalizer(gs, true);
     for(int i=0; i<gs.getTeams().length; i++) {
       for(int j=0; j<gs.getTeams()[i].getPieces().length; j++) {
-        assertTrue(! gsn.getNormalizedGameState().getTeams()[i].getPieces()[i].getId()
+        assertFalse(gsn.getNormalizedGameState().getTeams()[i].getPieces()[i].getId()
             .equals(gsn.getOriginalGameState().getTeams()[i].getPieces()[j].getId()));
       }
     }
   }
+  
+  @Test 
+  void testUnNormalizedMove() throws NoMovesLeftException, InvalidShapeException {
+    GameState gs = unnormalizedGameState();
+    GameStateNormalizer gsn = new GameStateNormalizer(gs, true);
+    
+    Move move = RandomAI.pickMoveComplex(gsn.getNormalizedGameState(), new ReferenceMove(null, new int[2])).toMove();
+    Move unmove = gsn.unnormalizeMove(move);
+    assertFalse(move.getPieceId().equals(unmove.getPieceId()));
+    assertArrayEquals(move.getNewPosition(), unmove.getNewPosition());
+    assertEquals(
+        unmove.getPieceId(),
+        gsn.getOriginalGameState()
+        .getTeams()[Integer.parseInt(move.getPieceId().split(":")[1].split("_")[0])]
+            .getPieces()[Integer.parseInt(move.getPieceId().split("_")[1])]
+                .getId()
+                );
+  }
 
+  /**
+   * creates an unnormalized GameState
+   * @return an unnormalized GameState
+   */
+  GameState unnormalizedGameState() {
+  //create unnormalized GameState
+    GameState gs = TestValues.getTestState();
+    for(int i=0; i<gs.getTeams().length; i++) {
+      gs.getTeams()[i].setId(funIds[i]);
+      for(int j=0; j<gs.getTeams()[i].getPieces().length; j++) {
+        gs.getTeams()[i].getPieces()[j].setId("p:"+ i + "_" + (int)(j+Math.random() * 80085));
+      }
+    }
+    
+    return gs;
+  }
 }
