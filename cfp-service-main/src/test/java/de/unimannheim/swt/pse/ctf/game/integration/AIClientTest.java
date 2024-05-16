713cd71b5b8ab2009f594920163080e26d85159d
Raffay Syed <raffay.syed@students.uni-mannheim.de>
2024-05-16 12:55:01 +0200

    #188 Local pas

---
+++ cfp-service-main/src/test/java/de/unimannheim/swt/pse/ctf/game/integration/AIClientTest.java
@@ -0,0 +1,75 @@
+package de.unimannheim.swt.pse.ctf.game.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.unimannheim.swt.pse.ctf.CtfApplication;
+import java.io.IOException;
+
+import org.ctf.shared.ai.AIConfig;
+import org.ctf.shared.client.AIClient;
+import org.ctf.shared.client.AIClientStepBuilder;
+import org.ctf.shared.client.lib.ServerDetails;
+import org.ctf.shared.client.lib.ServerManager;
+import org.ctf.shared.client.service.CommLayer;
+import org.ctf.shared.constants.Enums;
+import org.ctf.shared.constants.Enums.AI;
+import org.ctf.shared.constants.Enums.Port;
+import org.ctf.shared.state.data.map.MapTemplate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AIClientTest {
+
+  static CommLayer comm = new CommLayer();
+  final MapTemplate template = createGameTemplate();
+  ServerManager server = new ServerManager(comm, new ServerDetails("localhost", "9992"), template);
+
+  @BeforeAll
+  static void setup() {
+    String[] args = new String[] {"--server.port=9992"};
+    CtfApplication.main(args);
+  }
+
+  @Test
+  void AIClientInit() {
+    AIClient aiClient1 =
+        AIClientStepBuilder.newBuilder()
+            .enableRestLayer(false)
+            .onLocalHost()
+            .onPort("9992")
+            .aiPlayerSelector(AI.MCTS, new AIConfig())
+            .enableSaveGame(false)
+            .gameData(server.getGameSessionID(), "Team1")
+            .build();
+
+    AIClient aiClient2 =
+        AIClientStepBuilder.newBuilder()
+            .enableRestLayer(true)
+            .onRemoteHost("127.0.0.1")
+            .onPort(Port.AICLIENTTEST)
+            .aiPlayerSelector(AI.MCTS, new AIConfig())
+            .enableSaveGame(true)
+            .gameData(server.getGameSessionID(), "Team2")
+            .build();
+
+            try {
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+  }
+
+  private MapTemplate createGameTemplate() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    MapTemplate mapTemplate = null;
+    try {
+      mapTemplate =
+          objectMapper.readValue(
+              getClass().getResourceAsStream("/maptemplates/10x10_2teams_example.json"),
+              MapTemplate.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return mapTemplate;
+  }
+}
---
+++ cfp-service-main/src/test/java/de/unimannheim/swt/pse/ctf/game/integration/ServerManagerTest.java
@@ -0,0 +1,151 @@
+package de.unimannheim.swt.pse.ctf.game.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.unimannheim.swt.pse.ctf.CtfApplication;
+import java.io.IOException;
+
+import org.ctf.shared.client.lib.ServerDetails;
+import org.ctf.shared.client.lib.ServerManager;
+import org.ctf.shared.client.service.CommLayer;
+import org.ctf.shared.state.data.map.MapTemplate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ServerManagerTest {
+  static CommLayer comm = new CommLayer();
+
+  @BeforeAll
+  static void setup() {
+    String[] args = new String[] {"--server.port=9995"};
+    CtfApplication.main(args);
+  }
+
+  @Test
+  void testCreateGame() {
+    MapTemplate template = createGameTemplate();
+    ServerManager manager =
+        new ServerManager(comm, new ServerDetails("localhost", "9995"), template);
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+      fail();
+    }
+    assertNotNull(manager.gameSessionID);
+  }
+
+  @Test
+  void testDeleteGame() {
+    MapTemplate template = createGameTemplate();
+    ServerDetails serverDetails = new ServerDetails("localhost", "localhost");
+    serverDetails.setHost("localhost");
+    serverDetails.setPort("9995");
+    ServerManager manager =
+        new ServerManager(comm, serverDetails, template);
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+      fail();
+    }
+    assertNotNull(manager.gameSessionID);
+    try {
+      manager.deleteGame();
+    } catch (Exception e) {
+      fail();
+    }
+    assertFalse(manager.isSessionActive());
+  }
+
+  @Test
+  void testGetCurrentNumberofTeams() {
+    MapTemplate template = createGameTemplate();
+    ServerManager manager =
+        new ServerManager(comm, new ServerDetails("localhost", "9995"), template);
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+      fail();
+    }
+    assertNotNull(manager.gameSessionID);
+    try {
+      manager.getCurrentNumberofTeams();
+    } catch (Exception e) {
+      fail();
+    }
+    assertEquals(manager.getCurrentNumberofTeams(), 0);
+    comm.joinGame(
+        "http://"
+            + manager.serverDetails.getHost()
+            + ":"
+            + manager.serverDetails.getPort()
+            + "/api/gamesession/"
+            + manager.getGameSessionID(),
+        "TestTeam");
+    assertEquals(manager.getCurrentNumberofTeams(), 1);
+  }
+
+  @Test
+  void testGetGameSessionID() {
+    MapTemplate template = createGameTemplate();
+    ServerManager manager =
+        new ServerManager(comm, new ServerDetails("localhost", "9995"), template);
+    assertTrue(manager.serverDetails.isLocalhost());    
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+      fail();
+    }
+    ServerManager manager2 =
+    new ServerManager(comm, new ServerDetails("localhost", "9995"), manager.getGameSessionID());
+    manager2.getGameSessionID();
+    assertNotNull(manager2.getGameSessionID());
+    assertTrue(manager2.isSessionActive());
+  }
+
+  @Test
+  void deleteGameFalseTest() {
+    MapTemplate template = createGameTemplate();
+    ServerManager manager =
+        new ServerManager(comm, new ServerDetails("localhost", "9995"), template);
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+      fail();
+    }
+    manager.gameSessionID = "Empty";
+    assertFalse(manager.deleteGame());
+  }
+
+  @Test
+  void testServerNotActive() {
+    MapTemplate template = createGameTemplate();
+    ServerManager manager =
+        new ServerManager(comm, new ServerDetails("localhost", "9994"), template);
+    try {
+      manager.createGame();
+    } catch (Exception e) {
+    }
+    assertEquals(manager.getCurrentNumberofTeams(), 0);
+  }
+
+  private MapTemplate createGameTemplate() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    MapTemplate mapTemplate = null;
+    try {
+      mapTemplate =
+          objectMapper.readValue(
+              getClass().getResourceAsStream("/maptemplates/10x10_2teams_example.json"),
+              MapTemplate.class);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return mapTemplate;
+  }
+}
