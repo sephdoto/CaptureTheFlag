3e411dfe8bef6f2b9596ee308daadf790995ccb1
sistumpf <simon.stumpf@students.uni-mannheim.de>
2024-05-21 18:29:42 +0200

    [#274] Dokumentation und howManyMoves Methode eingefügt

--- shared/src/main/java/org/ctf/shared/gameanalyzer/GameAnalyzer.java
+++ shared/src/main/java/org/ctf/shared/gameanalyzer/GameAnalyzer.java
@@ -44,12 +44,23 @@ public class GameAnalyzer extends AIController {
     this.analyze = new AnalyzerThread(game, results);
   }
 
+  /**
+   * Analyzes the game in the background, allowing multitasking.
+   * Gets started on creation, so if the Thread is initialized it only stops when its done analyzing.
+   */
   private class AnalyzerThread extends Thread{
     SavedGame game;
     AnalyzedGameState[] results;
     int currentlyAnalyzing;
     boolean isAnalyzing;
 
+    /**
+     * Initialize with a SavedGame and an AnalyzedGameState array and the game will be analyzed.
+     * Results are put in results.
+     * 
+     * @param game SavedGame to analyze
+     * @param results AnalyzedGameState array to put the analyzing results into
+     */
     public AnalyzerThread(SavedGame game, AnalyzedGameState[] results) {
       this.game = game;
       this.results = results;
@@ -90,19 +101,39 @@ public class GameAnalyzer extends AIController {
       }
     }
     
-    public int getCurrentlyAnalyzing() {
+    protected int getCurrentlyAnalyzing() {
       return currentlyAnalyzing;
     }
 
-    public boolean isAnalyzing() {
+    protected boolean isAnalyzing() {
       return isAnalyzing;
     }
   }
   
+  /**
+   * Returns the index of the results array, which is currently being analyzed.
+   * Use together with {@link isAnalyzing()} to call the already analyzed moves.
+   * 
+   * @return index of currently analyzing move
+   */
   public int getCurrentlyAnalyzing() {
     return this.analyze.getCurrentlyAnalyzing();
   }
+  
+  /**
+   * Use this as an indicator if the analyzing Thread is running.
+   * super.isActive() works too, but this method should be used.
+   * 
+   * @return true if the game is currently being analyzed
+   */
   public boolean isAnalyzing() {
     return this.analyze.isAnalyzing();
+  }
+  
+  /**
+   * @return Returns how many moves were made in the saved game.
+   */
+  public int howManyMoves() {
+    return this.game.getMoves().size();
   }
 }
